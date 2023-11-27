63c3fc7d54c6e1fe441ad8bc728064c2efe28c1b
Awesomeash4 <137569158+Awesomeash4@users.noreply.github.com>
2023-11-27 21:22:38 +0530

    Update misc.py

--- scenario/modules/helper_funcs/misc.py
+++ scenario/modules/helper_funcs/misc.py
@@ -82,11 +82,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 5 : 5 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "⋘", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "❮", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("⨭", callback_data="all_cmds"),
+                EqInlineKeyboardButton("HOME", callback_data="all_cmds"),
                 EqInlineKeyboardButton(
-                    "⋙", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "❯", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
